d4357cbab556fbea73638c87ef769890cba60916
Your Name <you@example.com>
2019-03-29 16:20:40 +0800

    changes on some designs

--- simpleauth/views.py
+++ simpleauth/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
 
@@ -14,10 +13,6 @@ def registration(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            # username = form.cleaned_data.get('username')
-            # raw_password = form.cleaned_data.get('password1')
-            # user = authenticate(username=username, password=raw_password)
-            # login(request, user)
             return redirect('login')
     else:
         form = RegistrationForm()
